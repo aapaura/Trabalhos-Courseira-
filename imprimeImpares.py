ac8acd17fe9035d2e91b6c9b500c1d085645feda
Amanda Paura <35503324+aapaura@users.noreply.github.com>
2018-03-20 13:09:46 -0300

    Add files via upload

---
+++ imprimeImpares.py
@@ -0,0 +1,17 @@
+# entrando com o valor de n
+
+n = int(input("Digite o valor de n:"))
+
+# indices e variaveis
+
+num = n
+
+# loop
+
+while num > 0: # enquanto num for maior que zero
+    n = num % 2  # n recebe num modulo 2 (resto da divisao de num por 2)
+    if n != 0:  # se n for diferente de 0 (ou seja, se num for ímpar)
+        print(num)  # imprime num 
+    num -= 1  # num recebe num - 1 ---> loop
+        
+        
