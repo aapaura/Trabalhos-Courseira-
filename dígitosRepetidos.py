2a8d6b9d6244eeeab316827a613497f15184eee9
Amanda Paura <35503324+aapaura@users.noreply.github.com>
2018-03-20 13:05:41 -0300

    Update dígitosRepetidos.py

--- dígitosRepetidos.py
+++ dígitosRepetidos.py
@@ -1,7 +1,10 @@
-# achar dois numeros adjacentes iguais
-# 12334
-#   entrando com os dados numericos lidos como strings
+# Este programa procura em um número, se existem dois algarismos adjacentes que sejam iguais
+# ex: 12334 (repete o numero 3 duas vezes seguidas)
+
+
+#  entrando com n número - lido como strings
 num = input("Digite um número inteiro:")
+
 #   Lendo a quantidade de digitos das strings
 digitos = len(num)
 
@@ -9,20 +12,22 @@ digitos = len(num)
 num = int(num)
 digitos = int(digitos)
 
+# condição boolean 
 digitoRepetido = False
 
 #   Começando a verificação dos dígitos
+# verificando digito a digito enquanto não se acha um digito repetido
 while digitos > 0 and not digitoRepetido:
-    primeiroDigito = num % 10
-    num = num // 10
-    segundoDigito = num % 10
-    if primeiroDigito == segundoDigito:
-        digitoRepetido = True
-    else:
-        digitoRepertido = False
+    primeiroDigito = num % 10 # separando último digito do numero
+    num = num // 10  # dividindo o digito por 10 para pegar o penultimo digito (que será o novo ultimo)
+    segundoDigito = num % 10 # separando o penultimo digito (agora, novo último)
+    if primeiroDigito == segundoDigito: # comparando último com penultimo, se forem iguais, digitoRepetido recebe true (final do loop)
+        digitoRepetido = True 
+    else:  # se os digitos foram diferentes, digito recebe digito menos 1, volta para o loop com novo num (que foi dividido por 10)
+        digitoRepertido = False 
     digitos = digitos - 1
     
-#   Final do while; impressão dos resultados    
+#   Final do while; impressão dos resultados após término do loop
 if digitoRepetido:
     print("Este número tem digitos adjacentes iguais")
 else:
