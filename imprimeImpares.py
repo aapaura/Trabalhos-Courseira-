74f0059fc27750fa836fda45b1d30a4ab3992e09
Amanda Paura <35503324+aapaura@users.noreply.github.com>
2018-03-20 13:17:27 -0300

    Update imprimeImpares.py

--- imprimeImpares.py
+++ imprimeImpares.py
@@ -1,17 +1,17 @@
+# Verifica se um número é ímpar (ou se é par)
 # entrando com o valor de n
-
 n = int(input("Digite o valor de n:"))
 
 # indices e variaveis
-
 num = n
 
 # loop
-
 while num > 0: # enquanto num for maior que zero
     n = num % 2  # n recebe num modulo 2 (resto da divisao de num por 2)
     if n != 0:  # se n for diferente de 0 (ou seja, se num for ímpar)
-        print(num)  # imprime num 
+        print(num, "é um número ímpar") # imprime num
+    else: 
+        print(num, "é um número par")
     num -= 1  # num recebe num - 1 ---> loop
         
         
