7646e7e7974db3e18794cd8c2c04379af8ebeafd
Amanda Paura <35503324+aapaura@users.noreply.github.com>
2018-03-20 13:39:17 -0300

    Add files via upload

---
+++ sequenciaDecrescente.py
@@ -0,0 +1,20 @@
+# programa para achar sequencias de numeros decrescentes
+
+descrescente = True
+anterior = int(input("Digite o primeiro número da sequência:"))
+
+valor = 1
+
+while valor != 0 and descrescente:
+    valor = int(input("Digite o próximo número da sequência:"))
+    if valor > anterior:
+        descrescente = False
+    anterior = valor
+
+if descrescente:
+    print("A sequência está em ordem decrescente! :-)")
+else:
+    print("A sequência não está em ordem descrescente! :-(")
+    
+    
+        
