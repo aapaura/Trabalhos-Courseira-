9b385edd42b3bc6f6e4892ce3d44968359251b78
Amanda Paura <35503324+aapaura@users.noreply.github.com>
2018-03-20 13:22:18 -0300

    Update fatorial.py

--- fatorial.py
+++ fatorial.py
@@ -10,9 +10,9 @@ fatorial = 1
 n = num
 
 # loop
-while i < num: # 0 <  5 -- 1< 5 -- 2<5 -- 3<5 -- 4<5 -- 5<5 FALSE
-    fatorial *= n #1*5=5 -- 5*4=20 -- 20*3=60 -- 60*2=120 -- 120*1=120
-    n -= 1 #4 -- 3 -- 2 -- 1 -- 0
-    i += 1 #1 -- 2 -- 3 -- 4 -- 5
+while i < num: 
+    fatorial *= n 
+    n -= 1 
+    i += 1 
 
 print(fatorial)
