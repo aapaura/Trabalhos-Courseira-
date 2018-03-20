5ad3505e215177bf98f1d3c430557127f091b1b7
Amanda Paura <35503324+aapaura@users.noreply.github.com>
2018-03-20 13:19:48 -0300

    Update imprimeNimpares.py

--- imprimeNimpares.py
+++ imprimeNimpares.py
@@ -1,14 +1,13 @@
-# entrando com um número inteiro
-
-num = int(input("Digite o valor de n:"))
+# imprime os N primeiros números ímpares
+# entrando com um número inteiro N
+n = int(input("Digite o valor de n:"))
 
 # indices e variaveis
-
 i = 0
 impar = 1
 
 # loop
-while i < num:
+while i < n:
     print(impar)
     impar += 2
     i += 1
