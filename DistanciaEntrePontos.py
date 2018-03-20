819820d3e79f28174bdd64d49a9ff808644f2930
Amanda Paura <35503324+aapaura@users.noreply.github.com>
2018-03-20 12:51:19 -0300

    Update DistanciaEntrePontos.py

--- DistanciaEntrePontos.py
+++ DistanciaEntrePontos.py
@@ -1,15 +1,24 @@
+
+
+#>>>>>>  MEDINDO A DISTÂNCIA ENTRE DOIS PONTOS NO PLANO CARTESIANO  <<<<<<#
+
+#importando pacote math - funções matematicas
+
 import math
 
+#entrando com as coordenadas x e y dos pontos
 x1 = int(input("Entre com a coordenada x do primeiro número"))
 y1 = int(input("Entre com a coordenada y do primeiro número"))
 x2 = int(input("Entre com a coordenada x do segundo número"))
 y2 = int(input("Entre com a coordenada y do segundo número"))
 
+# calculo da distância entre dois pontos
 d = math.sqrt((( x1 - x2) ** 2)+((y1 - y2)**2))
 
-if d >= 10:
+# condições: 
+if d >= 10:  # se a distância for maior que dez, imprime que os pontos estão longe
     print("longe")
-else:
+else:  # se for menor que 10, imprime que estão perto
     print("perto")
 
 
