1c60940ff921c14e5ad2322fb1a2275d6663a0e3
Amanda Paura <35503324+aapaura@users.noreply.github.com>
2018-03-20 13:18:14 -0300

    Add files via upload

---
+++ imprimeNimpares.py
@@ -0,0 +1,16 @@
+# entrando com um número inteiro
+
+num = int(input("Digite o valor de n:"))
+
+# indices e variaveis
+
+i = 0
+impar = 1
+
+# loop
+while i < num:
+    print(impar)
+    impar += 2
+    i += 1
+    
+
